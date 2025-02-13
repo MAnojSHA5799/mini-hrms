e80cf960858e83ef646ce36af11abddcd9e2c085
MCAShishir23 <mcashishir23@gmail.com>
2025-02-13 16:37:19 +0530

    inti1

--- frontend/src/components/sidebar/componentsrtl/SidebarCard.jsx
+++ frontend/src/components/sidebar/componentsrtl/SidebarCard.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Container, Image } from 'react-bootstrap';
-import logo1 from '../../../assets/img/logo/logo1.png';
+// import logo1 from '../../../assets/img/logo/logo1.png';
 
 const FreeCard = () => {
   return (
     <div>
-  <Image className="h-36 w-15 rounded-full" src={logo1} alt="Logo" />
+  {/* <Image className="h-36 w-15 rounded-full" src={logo1} alt="Logo" /> */}
     </div>
   );
 };
